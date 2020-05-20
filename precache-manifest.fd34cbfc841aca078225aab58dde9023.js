c0d98b7ce303b46f2e962b7fc61fa74800da028c
ariessandi <ariessandi7@gmail.com>
2020-05-21 06:47:09 +0700

    Updates

---
+++ precache-manifest.fd34cbfc841aca078225aab58dde9023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75c0fe1d0474416f20b2af85b125844",
+    "url": "/spa-react/index.html"
+  },
+  {
+    "revision": "46fa14b9c2ab2ef41eb4",
+    "url": "/spa-react/static/css/main.22a76aee.chunk.css"
+  },
+  {
+    "revision": "f05345fa3e3d8709d349",
+    "url": "/spa-react/static/js/2.884bb6da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spa-react/static/js/2.884bb6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46fa14b9c2ab2ef41eb4",
+    "url": "/spa-react/static/js/main.7753b9bb.chunk.js"
+  },
+  {
+    "revision": "aa136160d7c975b67ed2",
+    "url": "/spa-react/static/js/runtime-main.12ff0bd0.js"
+  }
+]);
